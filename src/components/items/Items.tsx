b2c50fc9fd17ba408b63bcfdc0146316900c3958
Maria Riera <mgrr1214@gmail.com>
2024-06-17 22:57:16 -0400

    changes images for vercel

--- src/components/items/Items.tsx
+++ src/components/items/Items.tsx
@@ -7,7 +7,7 @@ const ItemComponent = ({ mini, data }: { mini: boolean; data: Item }) => {
   return (
     <div className={mini ? styles.mini : styles.normal}>
       <div className={styles.image}>
-        <img src={"./public/img/" + data.image} />
+        <img src={"/static/images/" + data.image} />
       </div>
       <p className={styles.title}>{data.title}</p>
       <p className={styles.price}>${data.price} USD</p>
